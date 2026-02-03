36198c82bbaaa477455aa57d5e72c0b378ef9fb0
Rohini Singh <rohiniv@chalmers.se>
2026-02-03 15:46:59 +0100

    varibel declaration

--- src/Scania.java
+++ src/Scania.java
@@ -1,8 +1,10 @@
 import java.awt.*;
 
 public class Scania extends Car{
+    private double FlatBedAngle;
     // Uppgift 1
     public Scania(){
         super(2, 550, Color.BLUE, "Scania");
+        FlatBedAngle = 0;
     }
 }
